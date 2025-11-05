44a7bfdbe0bd2cc54cf1c71126d42b5f383f5e0e
ddavidd <136312619+blu-rayy@users.noreply.github.com>
2025-11-05 14:04:47 +0800

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("Application started.");
+        logger2.log("User logged in.");
+
+        // Check if both references point to the same instance
+        System.out.println("Same instance? " + (logger1 == logger2)); // true
+    }
+}
